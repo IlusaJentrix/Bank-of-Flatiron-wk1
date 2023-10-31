31f791e056f28e7ba2944d8fa72973cf9cb85b6d
Jentrix <jentrix.ilusa@student.moringaschool.com>
2023-10-31 10:02:57 +0300

    Add the components

---
+++ src/components/Search.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Search() {
+    return (
+        <nav className="navbar bg-body-tertiary">
+            <div style={{justifyContent : "center"}} className="container-fluid">
+                <span className="navbar-text">
+                    <h1>Bank of Flatiron</h1>
+                </span>
+            </div>
+        </nav>
+    )
+}
+
+export default Search
+
+
+
+
