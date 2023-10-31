67796610fa2ffd8422cc24b09bd6031c2c593e52
Jentrix <jentrix.ilusa@student.moringaschool.com>
2023-10-31 12:28:26 +0300

    add app.js

---
+++ App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+import React, { useEffect, useState } from 'react';
+//import the created components 
+import TransactionForm  from './src/components/TransactionForm';
+import Search from './src/components/Search';
+import TransactionTable from './src/components/TransactionTable';
+
+function App() {
+  const [transactions, setTransactions] = useState([])
+
+  // Fetch data from the public API using useEffect
+
+  useEffect (()=> {
+    fetch ("https://my-json-server.typicode.com/Jentrix-2024/Bank-of-Flatiron-wk1/transactions")
+      .then ((r)=> r.json ())
+      .then ((data)=> setTransactions(data))
+  }, [])
+//update the new array to display
+  function updatedTransactions (newData) {
+    const updatedTransactionsArray = [...transactions, newData]
+    setTransactions (updatedTransactionsArray)
+  }
+//render the imported components to display on the app
+  return (
+    <div className="App">
+      <Search/><br/>
+      <TransactionForm newTransaction = {updatedTransactions}/><br/>
+      <TransactionTable arayOfAllTransactions = {transactions}/>
+    </div>
+  );
+}
+//export function
+export default App;
